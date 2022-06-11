d9752307c1ccada104d1dc67ed65870536a166e1
URSRAPID <98385291+URSRAPID@users.noreply.github.com>
2022-06-11 11:01:15 +0200

    MurNiveau1

--- Assets/Script/Controller/CharacterControler1.cs
+++ Assets/Script/Controller/CharacterControler1.cs
@@ -7,29 +7,39 @@ public class CharacterControler1 : MonoBehaviour
     [SerializeField] private PositionView positionView;
     private CharacterModel characterModel;
 
+    //[SerializeField] private float maxPos;
+    //[SerializeField] private float minPos;
 
     [SerializeField] private float speed = 5;
     [SerializeField] private float speedCam = 5;
 
+    Transform myTransform;
     void Start()
     {
-        characterModel = new CharacterModel(-10,0, 3, 3);
+        myTransform = GetComponent<Transform>();
+        characterModel = new CharacterModel(-10, 0, 3, 3);
         characterModel.GetLife().Subscribe(lifeView);
         characterModel.GetPosition().Subscribe(positionView);
     }
     void Update()
     {
-        characterModel.AddPosition(new Vector2(speedCam * Time.deltaTime, 0));
-        characterModel.AddPosition(new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-        Input.GetAxis("Vertical") * speed * Time.deltaTime));
 
+        float posX = Input.GetAxis("Horizontal");
+        float posY = Input.GetAxis("Vertical");
+        Vector2 newPos = new Vector2(posX * speed * Time.deltaTime,
+        posY * speed * Time.deltaTime);
+        characterModel.AddPosition(new Vector2(speedCam * Time.deltaTime, 0));
+        characterModel.AddPosition(newPos);
+        //myTransform.Translate(newPos);
+        //characterModel.AddPosition(new Vector2(Mathf.Clamp(characterModel.GetPosition().GetValue().x, minPos, maxPos ), characterModel.GetPosition().GetValue().y));
+        //myTransform.position = new Vector2(Mathf.Clamp(myTransform.position.x, minPos, maxPos), myTransform.position.y);
     }
 
     public void OnDamage()
     {
         Debug.Log("-1");
         characterModel.AddLife(-1);
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,7 +47,7 @@ public class CharacterControler1 : MonoBehaviour
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "BalleEnemy")
         {
             OnDamage();
-            
+
         }
     }
 }
