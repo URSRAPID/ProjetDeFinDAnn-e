fba1ece9efe128874d832d414ed511a6610470f9
URSRAPID <98385291+URSRAPID@users.noreply.github.com>
2022-06-27 15:52:28 +0200

    BoxIsActiveVagueLVL2

--- Assets/Wave1ReplayTrgger.cs
+++ Assets/Wave1ReplayTrgger.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class Wave1ReplayTrgger : MonoBehaviour
 {
-    public BoxCollider2D wave1;
-    public GameObject truc;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (wave1.isTrigger)
-        {
-            truc.SetActive(true);
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject vaque;
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-    
-        
+        if (collision.gameObject.tag == "BoxIsActiveVague")
+        {
+            vaque.gameObject.SetActive(true);
+        }
     }
 
 }
